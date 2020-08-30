67aa3f31f4d0d26793755b4f4c6c4ec473bada9c
harucandy9 <harucandy0913@gmail.com>
2020-08-30 16:17:00 +0900

    ログイン画面実装

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -7,9 +7,57 @@
 //
 
 import UIKit
+import Firebase
+
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var mailAdressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    //ログイン時
+    @IBAction func handleLoginButton(_ sender: Any) {
+    }
+    //アカウント作成時
+    @IBAction func handleCreateAccountButton(_ sender: Any) {
+        if let address = mailAdressTextField.text, let password = passwordTextField.text,
+            let displayName = displayNameTextField.text{
+            if address.isEmpty || password.isEmpty || displayName.isEmpty{
+                print("DEBUG_PRINT: 何かが空文字です。")
+                return
+            }
+            
+            //アドレスとパスワードでユーザ作成。
+            Auth.auth().createUser(withEmail: address, password: password){ authResult, error in
+                if let error = error {
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    return
+                }
+                print("DEBUG_PRINT: ユーザー作成に成功しました。")
+                
+                //表示名を設定する
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = displayName
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            //プロフィールの更新でエラーが発生
+                            print("DEBUG_PRINT: " + error.localizedDescription)
+                            return
+                        }
+                        print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                        
+                        //画面を閉じてタブ画面に戻る
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+                
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
